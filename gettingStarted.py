95b1e34e411102bbfaa2acd836dd4d60579a91ce
samgiska12 <123913416+samgiska12@users.noreply.github.com>
2023-02-04 21:57:34 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -25,23 +25,22 @@ if __name__ == "__main__":
 
 #Questions:
 if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-    print ("answer: str = "mTCP"")
+    answer = "mTCP"
 elif question == "Are encoding and encryption the same? - Yes/No":
-    print ("answer = "No"")
+    answer = "No"
 elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-    print ("answer = "No"")
+    answer = "No"
 elif question == "Is it possible to decode a message without a key? - Yes/No":
-    print ("answer = "Yes"")
+    answer = "Yes"
 elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-    print ("answer = "No"")
+    answer = "No"
 elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
-    print ("answer = """)
+    answer = ""
 elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-    print ("answer ="Yes"")
+    answer = "Yes"
 elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-    print ("answer = "7"")
+    answer = "7"
 elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-    print("answer = "3"")
+    answer = "3"
 else:
     print (welcome_assignment_answers(debug_question))
-return(answer)
