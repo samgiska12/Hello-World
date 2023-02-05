1b81f0eb3991b5536ea5c4130518818b8f04ccb0
samgiska12 <123913416+samgiska12@users.noreply.github.com>
2023-02-04 22:38:35 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,7 +16,7 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
-        answer = ""
+        answer = "883c13da6a24949c9a23231b60119e2ace58459da4f8bbdd812cc37764548bdd"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
