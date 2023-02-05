a1bc9df943a9a1391b184f14ede51f93e03124fc
samgiska12 <123913416+samgiska12@users.noreply.github.com>
2023-02-04 22:21:26 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,11 +18,11 @@ def welcome_assignment_answers(question):
     elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
         answer = ""
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "7"
+        answer = "5"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = "4"
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
